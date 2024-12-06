cfc54d4fec054cd93c4c1e56b6a1bfc09ba0f3ce
adrielsumathipala <adriel222@gmail.com>
2024-12-05 19:38:29 -0500

    fixing

---
+++ state_to_files.py
@@ -0,0 +1,48 @@
+STATE_TO_FILE_MAP = {
+    "Alabama": ["Copy of alison.legislature.state.al.us_code-of-alabama"],
+    "Alaska": ["Alaska-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Arizona": ["Arizona-DATA"],
+    "California": [
+        "California-DATA",
+        "California-DATA.xlsx (Converted - 2024-03-21 15:46)"
+    ],
+    "Colorado": ["Colorado-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Connecticut": ["Connecticut-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Delaware": ["Delaware-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Georgia": [
+        "Georgia-DATA",
+        "Georgia-DATA.xlsx (Converted - 2024-03-21 15:18)"
+    ],
+    "Hawaii": ["Hawaii-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Idaho": ["Idaho-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Illinois": ["Illinois-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Indiana": ["Indiana-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Kansas": ["Kansas-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Louisiana": ["Louisiana-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Maine": ["Maine-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Michigan": ["Michigan-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Minnesota": ["Minnesota-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Mississippi": ["Mississippi-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Missouri": ["Missouri-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Montana": ["Montana_DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Nebraska": ["Nebraska-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "New Hampshire": ["New Hampshire_DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "New Jersey": ["New Jersey-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "New York": ["New York-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "North Carolina": ["North Carolina_DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Ohio": ["Ohio-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Rhode Island": ["Rhode Island-DATA"],
+    "South Carolina": ["South Carolina-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "South Dakota": ["South Dakota_DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Tennessee": ["Tennessee-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Utah": ["UTAH-DATA"],
+    "Virginia": ["Virginia-DATA"],
+    "Washington": [
+        "Washington-DATA",
+        "Washington-DATA.xlsx (Converted - 2024-03-21 15:18)"
+    ],
+    "Washington DC": ["Washington DC"],
+    "West Virginia": ["West Virginia-DATA"],
+    "Wisconsin": ["Wisconsin-DATA.xlsx (Converted - 2024-03-21 15:18)"],
+    "Wyoming": ["Wyoming-DATA"]
+}
